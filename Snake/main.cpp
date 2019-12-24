031d870cf7aec3e44a927ca99b2da912b6ae7007
rainarit <rraina@ucsd.edu>
2019-12-24 13:09:12 +0530

    Initial Commit

---
+++ Snake/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Snake
+//
+//  Created by Ritik Raina on 12/24/19.
+//  Copyright © 2019 rraina. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
