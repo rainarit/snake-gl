eb74a3a05e193d14f8f6dd872318fa0f3557a6f6
rainarit <rraina@ucsd.edu>
2019-12-24 14:46:02 +0530

    Created game.cpp/.hpp

---
+++ Snake/game.cpp
@@ -0,0 +1,9 @@
+//
+//  game.cpp
+//  Snake
+//
+//  Created by Ritik Raina on 12/24/19.
+//  Copyright © 2019 rraina. All rights reserved.
+//
+
+#include "game.hpp"
---
+++ Snake/game.hpp
@@ -0,0 +1,14 @@
+//
+//  game.hpp
+//  Snake
+//
+//  Created by Ritik Raina on 12/24/19.
+//  Copyright © 2019 rraina. All rights reserved.
+//
+
+#ifndef game_hpp
+#define game_hpp
+
+#include <stdio.h>
+
+#endif /* game_hpp */
